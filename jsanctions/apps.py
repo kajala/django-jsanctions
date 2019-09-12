49771f83f6a0b0ce3c480a1f81f032545f763cd1
Jani Kajala <kajala@gmail.com>
2019-09-12 12:03:23 +0700

    unit tests

--- jsanctions/apps.py
+++ jsanctions/apps.py
@@ -2,6 +2,6 @@ from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
 
-class SanctionsConfig(AppConfig):
+class JsanctionsConfig(AppConfig):
     name = 'jsanctions'
     verbose_name = _('Sanction Lists')
--- jsanctions/tests.py
+++ jsanctions/tests.py
@@ -1,3 +1,15 @@
+import os
+from django.conf import settings
 from django.test import TestCase
+from jsanctions.management.commands.import_eu_sanctions import create_eu_sanctions
+from jsanctions.models import EuCombinedSanctionsList, SanctionEntity
 
-# Create your tests here.
+
+class Tests(TestCase):
+    def test_eu_sanctions_import(self):
+        print('test_eu_sanctions_import')
+        filename = os.path.join(settings.BASE_DIR, 'data/eu/20190827-FULL-1_1.xml')
+        source = EuCombinedSanctionsList.objects.create_from_filename(filename)
+        assert isinstance(source, EuCombinedSanctionsList)
+        create_eu_sanctions(source, verbose=False)
+        self.assertEqual(SanctionEntity.objects.all().count(), 2200)
