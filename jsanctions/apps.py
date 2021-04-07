71fdeb359d86d9e433b0e4546f0d57cb640f261c
Jani Kajala <kajala@gmail.com>
2021-04-07 13:42:35 -0500

    django 3.2 compatibility

--- jsanctions/apps.py
+++ jsanctions/apps.py
@@ -5,3 +5,4 @@ from django.utils.translation import gettext_lazy as _
 class JsanctionsConfig(AppConfig):
     name = "jsanctions"
     verbose_name = _("Sanction Lists")
+    default_auto_field = "django.db.models.AutoField"
